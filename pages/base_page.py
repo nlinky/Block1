16e7b572ff0fdf1386138e89ae331a01b2c5ba9f
nadya.chervyakova <kraynova06@yandex.ru>
2022-03-25 18:59:52 +0300

    pages and basepage

---
+++ pages/base_page.py
@@ -0,0 +1,26 @@
+from selenium.common.exceptions import NoSuchElementException
+from .locators import BasePageLocators
+
+
+class BasePage:
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def go_to_register_page(self):
+        register_link = self.browser.find_element()
+        register_link.click(*BasePageLocators.REGISTER_LINK)
+
+    def should_be_register_link(self):
+        assert self.is_element_present(*BasePageLocators.REGISTER_LINK), "Register link is not presented"
---
+++ pages/locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+
+class BasePageLocators:
+    REGISTER_LINK = (By.CSS_SELECTOR, '.ico-register')
